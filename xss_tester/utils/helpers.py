f427160d91b8c0e11dba6668acf1fb85bc9c0840
kitoi1 <kasaufx@gmail.com>
2025-05-27 17:21:26 +0300

    Update helpers.py

--- xss_tester/utils/helpers.py
+++ xss_tester/utils/helpers.py
@@ -1,1 +1,9 @@
+import requests
+
+def inject_payload(url, payload, timeout=5):
+    try:
+        response = requests.get(url + payload, timeout=timeout)
+        return payload in response.text
+    except Exception as e:
+        return False
 
