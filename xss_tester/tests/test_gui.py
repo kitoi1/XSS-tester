d62cd37ffe2de4fb536e2a641ef09e39a2dcf007
kitoi1 <kasaufx@gmail.com>
2025-05-27 17:20:58 +0300

    Update test_gui.py

--- xss_tester/tests/test_gui.py
+++ xss_tester/tests/test_gui.py
@@ -1,1 +1,9 @@
+import unittest
+
+class TestGUI(unittest.TestCase):
+    def test_dummy_gui(self):
+        self.assertEqual("KasauXSSAdvancedTester".lower(), "kasauxssadvancedtester")
+
+if __name__ == '__main__':
+    unittest.main()
 
